c2052840edfd8220ef7fc062f138b3e2a777ba47
zkan <kan@prontomarketing.com>
2019-09-06 08:51:16 +0700

    :white_check_mark: Add more test to check return value from get access token :bear:

--- test_program.py
+++ test_program.py
@@ -26,3 +26,22 @@ def test_get_access_token_should_call_correct_oauth_api_endpoint():
         get_access_token(username, password, auth_basic_token)
 
         mock_post.assert_called_once_with(OAUTH_API_ENDPOINT, data=payload, headers=headers)
+
+
+def test_get_access_token_should_return_access_token():
+    username = 'kan@pronto.com'
+    password = 'hellopronto'
+    auth_basic_token = '12345'
+
+    with ExitStack() as stack:
+        mock_data = {
+            'access_token': 'ACCESS_TOKEN',
+            'scope': 'global',
+            'token_type': 'bearer'
+        }
+        mock_post = stack.enter_context(patch('program.requests.post'))
+        mock_post.return_value.json.return_value = mock_data
+
+        access_token = get_access_token(username, password, auth_basic_token)
+
+        assert access_token == 'ACCESS_TOKEN'
